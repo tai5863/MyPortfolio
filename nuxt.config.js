ce05039aaed769740636592b5641ffa96145ce2c
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-05-21 17:41:01 +0900

    [UPDATE] デプロイ設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,24 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Taichi Uchida',
     htmlAttrs: {
       lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Taichi Uchida' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://ta1uchida.com' },
+      { hid: 'og:title', property: 'og:title', content: "Taichi Uchida's Portfolio" },
+      { hid: 'og:description', property: 'og:description', content: 'Visual Artist / Programmer. Affiliation : University of Tsukuba, Media Arts, Science and Technology. Keywords : Creative Coding / Visual Production / Computer Graphics / Web Engineering / Machine Learning.' },
+      { hid: 'og:image', property: 'og:image', content: '/img/MyProfile.img' },
+      { name: 'twitter:card', content: 'summary' },　//twitterの画像サイズ
+      { name: 'twitter:site', content: '@_t_ai__' },
+      { name: 'twitter:creator', content: '@_t_ai__' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
@@ -34,5 +44,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  mode: 'spa'
+  mode: 'spa',
+
+  target: 'static'
 }
